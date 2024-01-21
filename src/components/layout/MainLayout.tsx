032d5c0a668474ea9b1b6fea914a3465b41c1ffd
yungughyeon <djaudwls7873@gmail.com>
2024-01-21 16:26:16 +0900

    🌈feat : main layout 설정, router 설정

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,14 @@
+import MainFooter from 'components/ui/organisme/MainFooter/MainFooter'
+import MainHeader from 'components/ui/organisme/MainHeader/MainHeader'
+import React from 'react'
+
+const MainLayout = () => {
+	return (
+		<div>
+			<MainHeader />
+			<MainFooter />
+		</div>
+	)
+}
+
+export default MainLayout
---
+++ src/routes/router.tsx
@@ -0,0 +1,27 @@
+import MainLayout from 'components/layout/MainLayout'
+import AuthPage from 'pages/AuthPage'
+import MainPage from 'pages/MainPage'
+import NotFoundPage from 'pages/NotFoundPage'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+	{
+		element: <MainLayout />,
+		children: [
+			{
+				path: '/',
+				element: <MainPage />,
+			},
+		],
+	},
+	{
+		path: '/auth',
+		element: <AuthPage />,
+	},
+	{
+		path: '/*',
+		element: <NotFoundPage />,
+	},
+])
+
+export default router
