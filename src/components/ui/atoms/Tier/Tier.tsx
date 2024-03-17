f263e34319ec51c9bc1af3d4e8243b7f1836f5ff
yungughyeon <djaudwls7873@gmail.com>
2024-03-17 20:27:26 +0900

    🌈feat : main page rank 변경 (네온 효과 추가)

--- src/components/ui/atoms/Tier/Tier.tsx
+++ src/components/ui/atoms/Tier/Tier.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import TierImage from '../../../../assets/image/user/tier.png';
+import theme from 'styles/theme';
 
 const Tier = () => {
 	return (
 		<Container>
 			<Wrapper>
-				<Image src={TierImage} />
-				<span style={{ fontSize: '30px' }}>12357 위</span>
-				<span style={{ fontSize: '15px', color: '#DEE1E7' }}>184383 점</span>
+				<Ranking>12,000 위</Ranking>
+				<Update>2024년 01월 01일 13시 01분 기준</Update>
+			</Wrapper>
+			<Wrapper>
+				<SpanTitle>상위</SpanTitle>
+				<SpanContent>49%</SpanContent>
+				<SpanTitle>최고 점수</SpanTitle>
+				<SpanContent>19,999,000 점</SpanContent>
 			</Wrapper>
 		</Container>
 	);
@@ -23,14 +28,54 @@ const Container = styled.div`
 `;
 
 const Wrapper = styled.div`
-	width: 160px;
 	display: flex;
 	flex-direction: column;
-	align-items: center;
+	align-items: flex-start;
+	padding: 15px 0;
 	gap: 10px;
 `;
 
-const Image = styled.img`
-	width: 160px;
-	height: 200px;
+const Ranking = styled.span`
+	color: #ffffff;
+	text-shadow: 0 0 5px ${theme.PALETTE.secondary[100]},
+		0 0 10px ${theme.PALETTE.secondary[100]},
+		0 0 20px ${theme.PALETTE.secondary[100]},
+		0 0 40px ${theme.PALETTE.secondary[100]},
+		0 0 80px ${theme.PALETTE.secondary[100]},
+		0 0 90px ${theme.PALETTE.secondary[100]};
+	filter: blur(0.2px);
+	font-family: 'Roboto Mono';
+	font-style: normal;
+	font-weight: 700;
+	font-size: 58px;
+`;
+
+const Update = styled.span`
+	font-family: 'Roboto Mono';
+	font-style: normal;
+	font-weight: 700;
+	font-size: 14px;
+	color: #be98ae;
+	opacity: 0.2;
+`;
+
+const SpanTitle = styled.span`
+	font-family: 'Roboto Mono';
+	font-style: normal;
+	font-weight: 700;
+	font-size: 16px;
+	color: #fff;
+`;
+const SpanContent = styled.span`
+	font-family: 'Roboto Mono';
+	font-style: normal;
+	font-weight: 700;
+	font-size: 30px;
+	text-shadow: 0 0 5px ${theme.PALETTE.secondary[200]},
+		0 0 10px ${theme.PALETTE.secondary[200]},
+		0 0 20px ${theme.PALETTE.secondary[200]},
+		0 0 40px ${theme.PALETTE.secondary[200]},
+		0 0 80px ${theme.PALETTE.secondary[200]},
+		0 0 90px ${theme.PALETTE.secondary[200]};
+	filter: blur(0.2px);
 `;
