32bdfa50855d7252e8d654811788b2804a4d69c4
junghee.lee <junghee.lee@playlinks.com>
2024-03-18 21:01:27 +0900

    Merge branch 'main' of https://github.com/AimLab-Project/AimLab_Front
    (cherry picked from commit 30d421c19b9cf5f7f949bfe3d116d8d3d95e98e1)

--- src/components/ui/organisms/MainFooter/MainFooter.tsx
+++ src/components/ui/organisms/MainFooter/MainFooter.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import theme from 'styles/theme';
+import Discord from '../../../../assets/image/Logo/discord.png';
 
 const MainFooter = () => {
-	return <div>MainFooter</div>
-}
+	return (
+		<Container>
+			<Title>소셜 서비스</Title>
+			<Logo src={Discord} />
+			<ETC>버그 및 문의 : xxxxXXXX@gmail.com</ETC>
+		</Container>
+	);
+};
 
-export default MainFooter
+export default MainFooter;
+
+const Container = styled.div`
+	width: 100%;
+	height: 333px;
+	background-color: #000;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 20px;
+`;
+
+const Title = styled.span`
+	color: ${theme.PALETTE.fwhite};
+	font-weight: bold;
+	font-size: 35px;
+`;
+
+const Logo = styled.img`
+	width: 83px;
+	height: 83px;
+`;
+
+const ETC = styled.span`
+	color: ${theme.PALETTE.gray[700]};
+	font-weight: bold;
+	font-size: 20px;
+`;
---
+++ src/hooks/useRouter.ts
@@ -0,0 +1,26 @@
+import API_KEY from 'consts/API_KEY';
+import { useNavigate } from 'react-router-dom';
+
+const useRouter = () => {
+	const route = useNavigate();
+
+	// 스크롤 위치 0인 상태로 페이지 이동을 합니다
+	const toMove = (link: string) => {
+		window.scrollTo({ top: 0 });
+		route(link);
+	};
+
+	const moveMainPage = () => toMove(API_KEY.HOME);
+	const moveGamePage = () => toMove(API_KEY.GAME);
+	const moveMyDayaPage = () => toMove(API_KEY.MY_DATA);
+	const moveMyPage = () => toMove(API_KEY.MY_PAGE);
+
+	return {
+		moveMainPage,
+		moveGamePage,
+		moveMyDayaPage,
+		moveMyPage,
+	};
+};
+
+export default useRouter;
