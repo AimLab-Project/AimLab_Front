be2955152049cf3aee6ec1c9ccb37392a1aaf769
yungughyeon <djaudwls7873@gmail.com>
2024-04-27 21:50:46 +0900

    🚜test : 배포 테스트

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -8,40 +8,43 @@ import { createBrowserRouter } from 'react-router-dom';
 import OAuthPage from '../pages/OAuthPage';
 import MyPage from 'pages/MyPage';
 
-const router = createBrowserRouter([
-	{
-		element: <MainLayout />,
-		children: [
-			{
-				path: '/',
-				element: <MainPage />,
-			},
-			{
-				path: '/my',
-				element: <MyPage />,
-			},
-		],
-	},
-	{
-		path: '/auth/:page',
-		element: <AuthPage />,
-	},
-	{
-		path: '/oauth',
-		element: <OAuthPage />,
-	},
-	{
-		path: '/:provider/callback',
-		element: <OAuth2RedirectHandler />,
-	},
-	{
-		path: '/*',
-		element: <NotFoundPage />,
-	},
-	{
-		path: '/oauth/redirect',
-		element: <OAuth2RedirectHandler />,
-	},
-]);
+const router = createBrowserRouter(
+	[
+		{
+			element: <MainLayout />,
+			children: [
+				{
+					path: '/',
+					element: <MainPage />,
+				},
+				{
+					path: '/my',
+					element: <MyPage />,
+				},
+			],
+		},
+		{
+			path: '/auth/:page',
+			element: <AuthPage />,
+		},
+		{
+			path: '/oauth',
+			element: <OAuthPage />,
+		},
+		{
+			path: '/:provider/callback',
+			element: <OAuth2RedirectHandler />,
+		},
+		{
+			path: '/*',
+			element: <NotFoundPage />,
+		},
+		{
+			path: '/oauth/redirect',
+			element: <OAuth2RedirectHandler />,
+		},
+	],
+	{ basename: '/AimLab_Front' },
+);
 
 export default router;
